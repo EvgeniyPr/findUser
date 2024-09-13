1abe312d732499d81e4c6b3aa4ab7b944332cb1e
Evgenij P <prishchepovee@gmail.com>
2024-09-13 18:58:44 +0300

    refactoring

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,7 +2,7 @@ import css from "./Loader.module.css";
 export const Loader = () => {
   return (
     <>
-      <div style={{ textAlign: "center", marginTop: "50px" }}>Loarding...</div>
+      <div style={{ textAlign: "center", marginTop: "50px" }}>Loading...</div>
       <div className={css.loader}></div>
     </>
   );
